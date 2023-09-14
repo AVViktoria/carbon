982364e525a628bd924956d44d1586fa540a31b5
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-09-13 21:36:35 -0700

    fixed burger menu

--- js/menu.js
+++ js/menu.js
@@ -5,10 +5,26 @@
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
-
     mobileMenuRef.classList.toggle("is-open");
+    if (!expanded) {
+      document.body.style.overflowY = "hidden";
+    }
+  });
+
+  document.querySelectorAll("[data-menu]").forEach((n) =>
+    n.addEventListener("click", () => {
+      mobileMenuRef.classList.remove("is-open");
+      menuBtnRef.classList.remove("is-open");
+      document.body.style.overflowY = "visible";
+    })
+  );
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    menuBtnRef.classList.remove("is-open");
+    menuBtnRef.setAttribute("aria-expanded", false);
   });
 })();
